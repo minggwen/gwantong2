0dd89e1fa23c52d38c156140dd98bd387c4ee6dd
신민경 <tlsalsrud252@naver.com>
2024-03-15 13:27:10 +0900

    게시판

--- js/board_write.js
+++ js/board_write.js
@@ -1,9 +1,10 @@
-
+var boards = JSON.parse(localStorage.getItem("board"));
 document.getElementById('.enroll').addEventListener("click", function () {
     var title = document.getElementById('title-field').value;
-    var title = document.getElementById('title-field').value;
+    var content = document.getElementById('content-field').value;
+    var id = localStorage.getItem('login').id;
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-
+    
 });
