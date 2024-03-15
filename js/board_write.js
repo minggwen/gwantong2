58337e3983c54755efd2ad7312e10dd2b6d7a6a7
신민경 <tlsalsrud252@naver.com>
2024-03-15 12:16:42 +0900

    setting

---
+++ js/board_write.js
@@ -0,0 +1,9 @@
+
+document.getElementById('.enroll').addEventListener("click", function () {
+    var title = document.getElementById('title-field').value;
+    var title = document.getElementById('title-field').value;
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+
+});
